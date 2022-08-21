bc56cd1a94ef9e954a3e715b02af0acf1a5321e5
maedi <maediprichard@gmail.com>
2022-08-21 13:58:15 +1000

    use RubyTree

--- funnelcat.rb
+++ funnelcat.rb
@@ -1,9 +1,13 @@
 require 'csv'
+require 'tree'
 
 encoding = 'UTF-8'
 data = 'school_addresses.csv'
 
+root_node = Tree::TreeNode.new("ROOT", "FunnelCat")
+root_node.print_tree
+
 File.foreach(data, encoding: encoding) do |line|
   line = line.gsub(",", " ").delete('\-&"').strip.downcase.split
-  p line
+  # p line
 end
